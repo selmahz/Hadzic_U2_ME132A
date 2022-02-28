30c983b14413bcfd9fc98ebd183c399e7aa5186b
selma hadzic <selma.hadzic.se@gmail.com>
2022-02-28 15:52:25 +0100

    confusion part 2

--- main.js
+++ main.js
@@ -1,131 +1,99 @@
-"use strict"
+"use strict";
 
 //create new chracter and return it
-function createNewChar (name, house, gender, patronum, bloodstatus) {
+function createNewCharacter(name, house, gender, patronum, bloodstatus) {
     let character = {
         name: name,
         house: house,
         gender: gender,
         patronum: patronum,
-        bloodstatus: bloodstatus
-    }; 
+        bloodstatus: bloodstatus,
+    }
 
     return character;
 }
 
-
-//add new character to database
-function addCharToData (database, character) { 
-    database.push(character); 
+//add new dog to database 
+function addCharacterToData(database, character) {
+    database.push(character);
 }
 
 
-//removes character based on name (hantera character listan och ta bort id)
-function removeCharById(characters, id) {
-    for (let i = 0; i < characters.lenght; i++) {
-    let character = characters[i];
-
-    if (character.id == id) {
-        characters.splice(i, 1);
-        return;
-       }
+//remove character based on name from database
+function removeCharacterById(characters, id) {
+    for (let i = 0; i < characters.length; i++) {
+        let character = characters[i];
+        if (character.id == id) {
+            characters.splice(i, 1);
+            return;
+        }
     }
-} 
+}
 
 
-//return all characters based on house
-function getCharByHouse (database, house) {
-    let charByHouse = [];
+//return all characters based on house 
+function getCharactersByHouse(characters, house) {
+    let charactersByHouse = [];
 
     for (let character of characters) {
         if (character.house.toLowerCase() == house.toLowerCase()) {
-            charByHouse.push(character);
+            charactersByHouse.push(character);
         }
     }
 
-    return charByHouse;
+    return charactersByHouse;
 }
 
 
 //return all characters based on gender
-function getCharByGender (database, gender) {
-    let charByGender = [];
+function getCharactersByHouse(characters, gender) {
+    let charactersByGender = [];
 
     for (let character of characters) {
         if (character.gender.toLowerCase() == gender.toLowerCase()) {
-            charByGender.push(character);
+            charactersByGender.push(character);
         }
     }
 
-    return charByGender;
+    return charactersByGender;
 }
 
 
-//return all characters based on patronum 
-function getCharByPatronum (database, patronum) {
-    let charByPatronum = [];
-
-    for (let character of characters) {
-        if (character.patronum.toLowerCase() == patronum.toLowerCase()) {
-            charByPatronum.push(character);
-        }
-    }
-
-    return charByPatronum;
-}
-
-
-//return all characters based on bloodstatus 
-function getCharByBlood (characterList, bloodstatus) {
-    let charByBlood = [];
-
-    for (let character of database) {
-        if (character.bloodstatus.toLowerCase() == bloodstatus.toLowerCase()) {
-            charByBlood.push(character);
-        }
-    }
-
-    return charByBlood;
-}
-
-
-//render a character into html element
-function renderCharacter (character) {
+//render character into html element 
+function renderCharacter(character) {
     let div = document.createElement("div");
-    div.classList.add("HPchar");
-    div.id = HPchar.id; 
+    div.classList.add("character");
+    div.id = character.id;
 
     div.innerHTML = `
-        <div>${HPchar.name}</div>
-        <div>${HPchar.house}</div>
-        <div>${HPchar.gender}</div>
-        <div>${HPchar.patronum}</div>
-        <div>${HPchar.bloodstatus}</div>
-        <button type="button">Remove</button>
+    <div>${character.name}</div>
+    <div>${character.house}</div>
+    <div>${character.gender}</div>
+    <div>${character.patronum}</div>
+    <div>${character.bloodstatus}</div>
+    <button type="button">Remove</button>
     `;
 
     return div;
 }
 
 
-//render array of character into html 
-function renderCharacters(database) {
+//render array of characters into html
+function renderCharacters(characters) {
     let charactersElement = document.getElementById("HPchar");
     charactersElement.innerHTML = "";
 
-    //go through alla characters and unsert html
-    for (let character of database) {
+    for (let character of characters) {
         let characterElement = renderCharacter(character);
         charactersElement.appendChild(characterElement);
     }
 
-    //remove handlers for characters
-    setRemoveCharacterHandlers(); 
+    setRemoveCharacterHandlers();
 }
 
 
-// When <form id="add-dog-form"> is submitted
-function onAddCharacterSubmit(event) {
+//form add-char-form submitted 
+function addCharacterSubmit(event) {
     event.preventDefault();
 
     let name = document.getElementById("name").value;
@@ -134,91 +102,75 @@ function onAddCharacterSubmit(event) {
     let patronum = document.getElementById("patronum").value;
     let bloodstatus = document.getElementById("bloodstatus").value;
 
-    let character = createNewChar(name, house, gender, patronum, bloodstatus);
+    let character = createNewCharacter(name, house, gender, patronum, bloodstatus);
 
-    //calculate new id 
-    character.id = database[database.lenght - 1].id + 1;
+    character.id = database[database.length - 1].id + 1;
 
-    addCharToData(database, character);
+    addCharacterToData(database, character)
     renderCharacters(database);
 
-    //reset all fields in form 
     let form = document.getElementById("add-HPchar");
     form.reset();
 }
 
-
-//"click" event handler to button-add
 function setAddCharacterHandler() {
     let form = document.getElementById("add-HPchar");
-    form.addEventListener("submit", onAddCharacterSubmit);
+    form.addEventListener("submit", addCharacterSubmit);
 }
 
-// When user click remove-dog-button
-function onRemoveCharacterClick (event) {
+//when clicking the remove button
+function removeCharacterClick(event) {
     let button = event.target;
-    let id = button.parentElement.id;
-    removeCharById (database, id);
-    renderCharacters (database);
-}
+    let id = button.parentElement.id; 
 
-
-// Add "click" event handler to all remove-buttons
-function setRemoveCharacterHandlers() {
-    let buttons = document.querySelectorAll(".character button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", onRemoveCharacterClick);
-    }
-}
-
-// Filter by house
-function filterByHouseSubmit (event) {
-    event.preventDefault();
-
-    //what house?
-    let house = document.getElementById("filter-house").value;
-    //get by house
-    let characters = getCharByHouse(database, house);
-    //re-render
-    renderCharacters(characters);
-}
-
-
-// Filter by gender
-function filterByGenderubmit (event) {
-    event.preventDefault();
-
-    //what house?
-    let gender = document.getElementById("gender-house").value;
-    //get by house
-    let characters = getCharByHouse(database, gender);
-    //re-render
-    renderCharacters(characters);
-}
-
-
-//show all click
-function showAllClick() {
-    document.getElementById("filter-house").value = "";
-    document.getElementById("filter-gender").value = "";
-
+    removeCharacterById(database, id);
     renderCharacters(database);
 }
 
-//set filter
-function setFilterHandlers() {
+
+//add click event handler to all remove-buttons
+function setRemoveCharacterHandlers() {
+    let buttons = document.querySelectorAll(".character button");
+
+     for (let button of buttons) {
+         button.addEventListener("click", removeCharacterClick);
+    }
+}
+
+
+//filter by house 
+function filterByHouse(event) {
+    event.preventDefault();
+    let house = document.getElementById("filter-house").value;
+    let characters = getCharactersByHouse(database, house);
+    renderCharacters(characters);
+}
+
+//filter by gender
+function filterByGender(event) {
+    event.preventDefault();
+    let gender = document.getElementById("filter-gender").value;
+    let characters = getCharactersByGender(database, gender);
+    renderCharacters(characters);
+}
+
+function onShowAllClick() {
+    document.getElementById("filter-house").value = "";
+    document.getElementById("filter-gender").value = "";
+    renderCharacters(database);
+}
+
+function setFilterCharacterHandlers() {
     let houseForm = document.getElementById("filter-by-house");
     let genderForm = document.getElementById("filter-by-gender");
     let showAll = document.getElementById("show-all");
 
-    houseForm.addEventListener("submit", filterByGenderubmit);
-    genderForm.addEventListener("submit", filterByGenderubmit);
-    showAll.addEventListener("click", showAllClick);
+    houseForm.addEventListener("submit", filterByHouse);
+    genderForm.addEventListener("submit", filterByGender);
+    showAll.addEventListener("click", onShowAllClick);
 }
 
-
-//adjust the page 
+//adjust the page
 renderCharacters(database);
 setAddCharacterHandler();
-setFilterHandlers();
+setFilterCharacterHandlers();
