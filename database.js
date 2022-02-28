5e29d1f345f9b1125f0f530c0f2b10d60d176571
selma hadzic <selma.hadzic.se@gmail.com>
2022-02-28 11:50:04 +0100

    database created

---
+++ database.js
@@ -0,0 +1,92 @@
+//harry potter characters database//
+
+let database = [
+
+{ Id: 1,
+    Name: "Harry Potter",
+    House: "Gryffindor",
+    Patronum: "Stag",
+    Bloodstatus: "Half-blood"
+},
+
+
+{
+    Id: 2,
+    Name: "Hermione Granger",
+    House: "Gryffindor",
+    Patronum: "Otter",
+    Bloodstatus: "Muggle-born"
+},
+
+
+{
+    Id: 3,
+    Name: "Ron Weasley",
+    House: "Gryffindor",
+    Patronum: "Jack Russell Terrier",
+    Bloodstatus: "Pure-blood",
+},
+
+
+{ 
+    Id: 4, 
+    Name: "Luna Lovegood",
+    House: "Ravenclaw",
+    Patronum: "Hare",
+    Bloodstatus: "Pure-blood"
+},
+
+
+{ 
+    Id: 5,
+    Name: "Draco Malfoy",
+    House: "Slytherin",
+    Patronum: "None",
+    Bloodstatus: "Pure-blood",
+},
+
+{ 
+    Id: 6, 
+    Name: "Sirius Black",
+    House: "Gryffindor",
+    Patronum: "Non-corporeal",
+    Bloodstatus: "Pure-blood",
+},
+
+
+{ 
+    Id: 7,
+    Name: "Remus Lupin",
+    House: "Gryffindor",
+    Patronnum: "Wolf",
+    Bloodstatus: "Half-blood",
+},
+
+
+{ 
+    Id: 8,
+    Name: "Severus Snape",
+    House: "Slytherin",
+    Patronum: "Doe",
+    Bloodstatus: "Half-blood",
+},
+
+
+{ 
+    Id: 9,
+    Name: "Tom Riddle / Voldemort",
+    House: "Slytherin",
+    Patronum: "None",
+    Bloodstatus: "Half-blood",
+},
+
+
+{ 
+    Id: 10,
+    Name: "Nymphadora Tonks",
+    House: "Hufflepuff",
+    Patronum: "Jack Rabbit (pre-1995), wolf (1995-1998)",
+    Bloodstatus: "Half-blood",
+},
+
+];
